7a9793bbaf10befb4fea25d775d43cfc2ef95e23
Tanishk <tanishkr006@gmail.com>
2026-01-03 15:31:40 +0530

    setup: initalize express server and database connection

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// this is where the server is made
+
+// to configure environmental variables 
+
+const dotenv = require('dotenv');
+dotenv.config();  
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+// this is a temporary route
+app.get('/',(req , res) => {
+  res.send("Hello World")
+});
+
+module.exports = app ;
