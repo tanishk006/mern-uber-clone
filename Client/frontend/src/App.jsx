1d2134a41c2c4bc471fde596d18367e578ae406e
Tanishk <tanishkr006@gmail.com>
2026-01-09 18:08:36 +0530

    fix: imported the files for Routes

--- Client/frontend/src/App.jsx
+++ Client/frontend/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import {Route ,Routes} from 'react-router-dom'
+import Home from "./pages/home/Home";
+import UserLogin from "./pages/userlogin/UserLogin";
+import UserSignup from "./pages/usersignup/UserSignup";
+import CaptainLogin from "./pages/captainlogin/CaptainLogin";
+import CaptainSignup from "./pages/captainsignup/CaptainSignup";
+
+
 
 const App = () => {
   return (
@@ -7,12 +14,13 @@ const App = () => {
          <Routes> 
   
            <Route path='/' element={<home />} />
-           <Route path='/login' element={<captainLogin />} />
+           <Route path='/captain-login' element={<captainLogin />} />
            <Route path='/login' element={<userlogin />} />
            <Route path='/signup' element={<userSignUp />} />
-           <Route path='/signup' element={<captainSignup />} />
+           <Route path='/captain-signup' element={<captainSignup />} />
 
           </Routes>
+          <div>App</div>
     </div>
   )
 }
