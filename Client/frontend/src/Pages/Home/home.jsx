68964c51224ff1c8afefdd9c3005e70248f8ffab
Tanishk <tanishkr006@gmail.com>
2026-01-10 00:00:07 +0530

    chore: Configured the logo

--- Client/frontend/src/Pages/Home/home.jsx
+++ Client/frontend/src/Pages/Home/home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import logo from "../../assets/logo.png";
+
+
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+      <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
+      <img src={logo} alt="Uber Logo" className="bg-transparent w-32" />
+          <div className='bg-white'>
+             <h2>Get started with Uber </h2>
+          <button>Continue</button>
+          </div>
+      </div>
+    </div>
   )
 }
 
